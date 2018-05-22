1e3279458d48f40c1bcee196f046a5b8ae41a6bf
DaiJitao <976185561@qq.com>
2018-05-23 00:16:17 +0800

    add

--- src/person/test/tree/TreeApp.java
+++ src/person/test/tree/TreeApp.java
@@ -5,6 +5,14 @@ package person.test.tree;
  */
 public class TreeApp {
     public static void main(String[] args) {
+        int i = 0;
+        try {
+            int m = 4 / i;
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("error!");
+        }
+        System.out.println("1111111111111111111");
         
     }
     public Node find(int key){
