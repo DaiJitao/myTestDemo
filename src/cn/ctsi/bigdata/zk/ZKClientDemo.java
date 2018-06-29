84d5f19568a717e7d052fe0582ac5e055b2c0db1
daijitao <daijitao@ctsi.com.cn>
2018-06-29 20:00:08 +0800

    提交代码

--- src/cn/ctsi/bigdata/zk/ZKClientDemo.java
+++ src/cn/ctsi/bigdata/zk/ZKClientDemo.java
@@ -1,6 +1,6 @@
 package cn.ctsi.bigdata.zk;
 
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,10 +10,26 @@ import java.util.List;
  * Created by daijitao on 2018/6/29.
  */
 public class ZKClientDemo {
-    public static void main(String[] args) throws IOException {
+    static class ClientBase{
+        static int CONNECT_TIMEOUT = 2000;
+    }
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
         String hostPort = "localhost:2181";
         String zpath = "/";
+
         List<String> zooChildren = new ArrayList<>();
+        Watcher watcher = new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                System.out.println("已经触发了" + event.getType() + " 事件!");
+            }
+        };
+
+        ZooKeeper zkCLient = new ZooKeeper("localhost:2181", ClientBase.CONNECT_TIMEOUT, watcher);
+        zkCLient.create("/daijitao/hello", "hello daijitao".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        //zkCLient.create("/daijitao", "hello daijitao".getBytes(), -1);
+        System.out.println(zkCLient.getData("/daijitao", watcher, null));
+        zkCLient.close();
 
     }
 }
