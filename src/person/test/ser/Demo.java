768636a3e1a7ce8890a578234bffbc41ca32c8ad
DaiJitao <976185561@qq.com>
2018-09-29 22:33:13 +0800

    asdfghjkl

---
+++ src/person/test/ser/Demo.java
@@ -0,0 +1,16 @@
+package person.test.ser;
+
+public class Demo {
+    public static void main(String[] args) {
+        byte a = 127;
+        int b1 = 128;
+        int b2[] = new int[12];
+        System.out.println(b2[1]);
+
+        Integer b3 = new Integer(128);
+        boolean is = b3.equals(128);
+        System.out.println(is);
+
+
+    }
+}
