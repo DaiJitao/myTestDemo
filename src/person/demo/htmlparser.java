5549d431808226a5c0682937a860d5a879ec454c
DaiJitao <976185561@qq.com>
2018-09-29 23:11:08 +0800

    asdfghjkl

--- src/person/demo/htmlparser.java
+++ src/person/demo/htmlparser.java
@@ -3,5 +3,19 @@ package person.demo;
 /**
  * Created by daijitao on 2018/8/31.
  */
-public class htmlparser {
+public class htmlparser extends A{
+    public htmlparser(A a){
+        System.out.println("B");
+    }
+
+    public static void main(String[] args) {
+        A a = new A();
+        htmlparser htmlparser = new htmlparser(a);
+    }
+}
+
+class A {
+    public A(){
+        System.out.println("A");
+    }
 }
