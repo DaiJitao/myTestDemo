aadb623ceb12232fc2969ba17bd0c65ddfde068b
daijitao <daijitao@ctsi.com.cn>
2018-10-09 16:19:08 +0800

    提交代码

--- src/person/demo/htmlparser.java
+++ src/person/demo/htmlparser.java
@@ -1,7 +1,27 @@
 package person.demo;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by daijitao on 2018/8/31.
  */
 public class htmlparser {
+    List<String> a = new ArrayList<>();
+    public static void main(String[] args) {
+
+        Runnable arss = () -> System.out.println("Hello world");
+        Thread thread = new Thread(arss);
+        thread.start();
+        System.out.println("test");
+
+
+
+
+    }
+}
+
+class Artist<E> extends ArrayList{
+
 }
--- src/person/demo/jvm/TestSingleton.java
+++ src/person/demo/jvm/TestSingleton.java
@@ -3,6 +3,7 @@ package person.demo.jvm;
 /**
  * Created by daijitao on 2018/9/5.
  */
+// E:\intell_space\myTestDemo\out
 public class TestSingleton {
     public static void main(String args[]){
         Singleton singleton = Singleton.getSingleton();
