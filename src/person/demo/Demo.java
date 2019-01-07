852c83137df215261f8087768d459196dd8b2288
daijitao <daijitao@ctsi.com.cn>
2019-01-07 14:02:16 +0800

    add

--- src/person/demo/Demo.java
+++ src/person/demo/Demo.java
@@ -1,10 +1,24 @@
 package person.demo;
 
+import java.util.Scanner;
+
 /**
  * Created by daijitao on 2018/11/22.
  */
 public class Demo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str;
+        while (true){
+            str = scanner.next();
+            str = str.replace("吗", "");
+            str = str.replace("?", "!");
+            str = str.replace("?", "!");
+            System.out.println(str);
+        }
+    }
+
+    public static void main1(String[] args) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("abc");
         change(stringBuilder);
